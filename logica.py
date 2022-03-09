7010315f1ac345dad89bb2c3027b41f9035c38eb
patandrade09 <patricia.andrade094@gmail.com>
2022-03-09 15:02:40 -0300

    first commit

---
+++ logica.py
@@ -0,0 +1,7 @@
+#Lógica
+#Definir os jogadores - jogador 1 e jogador2
+#Para o Jogador 1 - será atribuído o valor X
+#Para o Jogador 2 - será atribuído o valor O
+#Definir as 9 posições do jogo
+#Definir as 8 posições de Ganhar
+
---
+++ velha.py
@@ -0,0 +1,17 @@
+class JogoDaVelha:
+    def __init__(self, user1, user2):
+        user1 = input("Escolha X ou O")
+        if user1 == "X":
+            user2 == "O"
+
+    def positions(marcar):
+        matriz = [["_","_","_"],
+              ["_","_","_"],
+              ["_","_","_"]]
+
+jogo1 = JogoDaVelha(1,2)
+    
+
+
+
+    
