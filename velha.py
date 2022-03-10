cb7830adbcd32e6504ba04f9958c3921fb916f88
patandrade09 <patricia.andrade094@gmail.com>
2022-03-10 15:00:01 -0300

    adicionando métodos essenciais e inicializando as iterações

--- velha.py
+++ velha.py
@@ -6,7 +6,7 @@ class JogoDaVelha:
 #O None é uma palavra-chave especial em Python. Isso não significa que o valor é zero, mas o valor é NULL ou não disponível. 
     def __init__(self, user1="X"):
         self.jogada = user1
-# criamos então o jogo na ordem de teclado numérico em forma de DICIONARIO
+# criamos então o jogo na ordem de teclado numérico em forma de DICIONARIO {chave : valor}
         jogo = {"7":" ", "8":" ", "9":" ",
                      "4":" ", "5":" ", "6":" ",
                      "1":" ", "2":" ", "3":" "}
@@ -30,16 +30,58 @@ class JogoDaVelha:
         else:
             return False
 
-# método de dicionários .keys serve para obter todas as chaves de um dicionário, no caso,retornar 7,8,9
-# agora vamos ao jogo de fato! o método verifica o ESTADO do tabuleiro, verificando se o jogador preencheu três casas consecutivas.
-# https://www.covildodev.com.br/artigo/jogo-da-velha-python
-    
+# método de dicionários .keys serve para obter todas as chaves de um dicionário, no caso, retornar 7,8,9
+
+
+# metodos essencial: o método verifica o ESTADO do tabuleiro, verificando se o jogador preencheu três casas consecutivas.
+    def verifica_jogo(self):
+        # verificando as 3 posições verticais
+        if self.jogo["7"] == self.jogo["4"] == self.jogo["1"] != " ":
+            return self.jogo["7"] 
+            #aqui ele ta retornando o jogador que ganhou, se foi O ou X. 
+        elif self.jogo["8"] == self.jogo["5"] == self.jogo["2"] != " ":
+            return self.jogo["8"]
+        elif self.jogo["9"] == self.jogo["6"] == self.jogo["3"] != " ":
+            return self.jogo["9"]
         
+        # verificando as três posições horizontais
+        elif self.jogo["7"] == self.jogo["8"] == self.jogo["9"] != " ":
+            return self.jogo["7"]
+        elif self.jogo["4"] == self.jogo["5"] == self.jogo["6"] != " ":
+            return self.jogo["4"]
+        elif self.jogo["1"] == self.jogo["2"] == self.jogo["3"] != " ":
+            return self.jogo["1"]
+        
+        #verificando as duas posições diagonais
+         # diagonal contra-barra
+        elif self.jogo["7"] == self.jogo["5"] == self.jogo["3"] != " ":
+            return self.jogo["7"]
+         # diagonal barra
+        elif self.jogo["9"] == self.jogo["5"] == self.jogo["1"] != " ":
+            return self.jogo["9"]
 
+# agora vamos verificar se o jogo deu empate
+        if [*self.jogo.values()].count(" ") == 0:
+            return "Jogo Empatado"
+        else:
+            return [*self.jogo.values()].count(" ")
+# aqui foi feito o desempacotamento dos VALUES do dicionário JOGO e em seguida e contagem desses values que estão com espaço em branco. 
+# Caso algum jogador tenha preenchido 3 casas consecutivas é retornado o vencedor.
+# Caso a condição acima não ocorra, e caso todas as casas já estejam ocupadas(a quantidade de casas vazias seja igual a zero), é retornado empate.
+# Se nem uma das duas condições forem verdadeiras, é retornado a quantidade de casas que ainda faltam serem preenchidas.
 
+# agora vamos ao jogo de fato! 
+    def jogar(self):
 
-    
+        jogo = self.mostra_jogo
+        for jogando in jogo:
 
+            if jogando == True:
+                print(f"Turno do {self.jogada}, qual sua jogada?")
+        
+        # Enquanto o jogador não fizer uma jogada válida
+            vai_jogar = input("Jogada: ")
+            for aguardando in vai_jogar:
+                if aguardando == True:
+                    return self.valida_jogada[vai_jogar]
 
-
-    
